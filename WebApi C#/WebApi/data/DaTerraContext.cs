4fdd527496965aaa9702bc426e637e4e7e1024c6
Gabriel_Lopes <53120770+Gabrielll-sys@users.noreply.github.com>
2022-10-21 21:28:01 -0300

    Primeira WebApi em .NET

---
+++ WebApi C#/WebApi/data/DaTerraContext.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApiDaTerra.data;
+using Microsoft.EntityFrameworkCore;
+using WebApiDaTerra.models;
+
+
+public class DaTerraDbContext:DbContext
+{
+    public DbSet<Produto> Produtos { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlite("DataSource=app.db;Cache=Shared");
+}
